1e72875164b38013c42032b6826dc12b4f43f360
Oladigbs18 <aaliyaholadigbolu@gmail.com>
2023-08-20 23:49:03 +0100

    fix: compile error

--- main.h
+++ main.h
@@ -2,12 +2,12 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdarg.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <unistd.h>
 #include <limits.h>
 
-int _printf(const char *format, ...);
 int _putchar(char c);
+int _printf(const char *format, ...);
 
 #endif /* MAIN_H */

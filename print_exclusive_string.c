10123c9641afb9d49ba4884eeacc6095db7cf5e7
Abdulrasaq Jinadu <eniolajinadu2003@gmail.com>
2023-08-26 14:49:44 -0700

    corrupt

--- print_exclusive_string.c
+++ print_exclusive_string.c
@@ -9,16 +9,15 @@ int printf_exclusive_string(va_list val)
 {
 		char *s;
 		int i, len = 0;
-		int cast;
-		
+		int cast;	
 		s = va_arg(val, char *);
 		if (s == NULL)
 			s = "(null)";
 		for (i = 0; s[i] != '\0'; i++)
-		{												if (s[i] < 32 || s[i] >= 127)
-												{
-																			_putchar('\\');
-																						_putchar('x');
+		{
+			if (s[i] < 32 || s[i] >= 127)
+			{
+				_putchar('\\');																						_putchar('x');
 																									len = len + 2;
 																												cast = s[i];
 																															if (cast < 16)
@@ -26,7 +25,7 @@ int printf_exclusive_string(va_list val)
 																																								_putchar('0');
 																																												len++;
 																																															}
-																																		len = len + printf_HEX_aux(cast);
+																																		len = len + printf_HEX(cast);
 																																				}
 														else
 																	{
